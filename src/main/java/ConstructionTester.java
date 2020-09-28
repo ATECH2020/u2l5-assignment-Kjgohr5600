9a0ab92ac08e0107a2a851920e67de0c21b0c1f6
Kyle Gohr <kyle.279965@nv.ccsd.net>
2020-09-28 06:09:10 +0000

    construction tester

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -2,8 +2,27 @@ import java.util.Scanner;
 
 public class ConstructionTester
 {
-    public static void main(String[] args)
-    {
-        // Start here!
-    }
+    public static void main(String[] args)
+    {
+      Scanner in = new Scanner(System.in);
+      double lumberCost = 8;
+      double windowCost = 11;
+      double taxRate;
+      int boards, windows;
+     
+     //asks user information to calc total
+      System.out.println("Enter the sales tax rate: ");
+      taxRate = in.nextDouble();
+      System.out.println("How many boards do you need?");
+      boards = in.nextInt();
+      System.out.println("How many windows do you need?");
+      windows = in.nextInt();
+
+      Construction bobby = new Construction(lumberCost,windowCost,taxRate);
+
+      total = bobby.lumberCost(boards) + bobby.windowCost(windows);
+      System.out.println("Total: " + total);
+
+      System.out.println("Grand Total:" + bobby.grandTotal(total));
+    }
 }
