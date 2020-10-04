c678ff1db78cd64792afc3966e43c1f7ec8f18e2
Kyle Gohr <kyle.279965@nv.ccsd.net>
2020-10-04 19:35:14 +0000

    finished assignment and did bug fixes

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -2,27 +2,37 @@ import java.util.Scanner;
 
 public class ConstructionTester
 {
-    public static void main(String[] args)
-    {
-      Scanner in = new Scanner(System.in);
-      double lumberCost = 8;
-      double windowCost = 11;
-      double taxRate;
-      int boards, windows;
-     
-     //asks user information to calc total
-      System.out.println("Enter the sales tax rate: ");
-      taxRate = in.nextDouble();
-      System.out.println("How many boards do you need?");
-      boards = in.nextInt();
-      System.out.println("How many windows do you need?");
-      windows = in.nextInt();
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner (System.in);
+        
+        //variables
+        int numBoards;
+        int numWindows;
+        double taxRate;
+        double total, grandTotal;
+        double lumCost = 8;;
+        double winCost = 11;
+      
 
-      Construction bobby = new Construction(lumberCost,windowCost,taxRate);
+        System.out.print("Enter the sales tax rate: ");
+        taxRate = scanner.nextDouble();
+        taxRate = in.nextDouble();
+        
+        System.out.println("How many boards do you need? ");
+        numBoards = scanner.nextInt();
+        numBoards = in.nextDouble();
 
-      total = bobby.lumberCost(boards) + bobby.windowCost(windows);
-      System.out.println("Total: " + total);
+        System.out.println("How many windows do you need? ");
+        numWindows = scanner.nextInt();
 
-      System.out.println("Grand Total:" + bobby.grandTotal(total));
-    }
-}
+
+        Construction myConstruction = new Construction (taxRate, lumCost, winCost);
+
+        total = test.lumCost (numBoards) + test.winCost (numWindows);
+        System.out.print("The total is: " + total);
+
+        grandTotal = test.grandTotal (total);
+        System.out.print("The grand total is: " + grandTotal);
+    }//ends main method
+}//ends class
